10838856399f8d7b3400891970bda6bdfa2dbd12
Albeiro <albeiroespitiasierra@gmail.com>
2018-10-31 13:57:28 -0500

    Agregando mas pruebas a user

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,18 +1,22 @@
 function LoginController() {
 
-  function isValidUserId(userList, user) {
-    return userList.indexOf(user) >= 0;
-  }
+    function isValidUserId(userList, user) {
+        return userList.indexOf(user) >= 0;
+    }
 
-  function isEmailValid(userEmail) {
-    return /@gmail\.com$/.test(userEmail)
-  }
+    function isEmailValid(userEmail) {
+        return /@gmail\.com$/.test(userEmail)
+    }
 
-  return {
-    isValidUserId,
-    isEmailValid
-  }
+    function isPasswordLengthGreaterThan8(userPassword) {
+        return userPassword.length > 8
+    }
 
+    return {
+        isValidUserId,
+        isEmailValid,
+        isPasswordLengthGreaterThan8
+    }
 }
 
 module.exports = LoginController();
--- test/controllers/login.controller.spec.js
+++ test/controllers/login.controller.spec.js
@@ -3,28 +3,38 @@ const loginController =  require('../../controllers/login.controller');
 
 describe('LoginController', function () {
 
-  describe('isValidUser', function(){
+    describe('isValidUser', function(){
 
-    it('debe retornar verdadero si el id es valido', function(){
-      let isValid = loginController.isValidUserId(['abc123','xyz321'], 'abc123')
-      assert.equal(isValid, true);
+        it('deberia retornar verdadero si el id es valido', function(){
+            let isValid = loginController.isValidUserId(['abc123','xyz321'], 'abc123')
+            assert.equal(isValid, true);
+        });
+
+        it('deberia retornar falso si el id es invalido', function(){
+            let isValid = loginController.isValidUserId(['abc123','xyz321'],'abc1234')
+            assert.equal(isValid, false);
+        });
+
+        it('deberia retornar verdadero si el email es valido', function(){
+            let isValid = loginController.isEmailValid('albeiro@gmail.com')
+            assert.equal(isValid, true);
+        });
+
+        it('deberia retornar falso si el email es invalido', function(){
+            let isValid = loginController.isEmailValid('albeiro.com')
+            assert.equal(isValid, false);
+        });
+
+        it('deberia retornar verdadero si la contraseña tiene mas de 8 caracteres', function(){
+            let isValid = loginController.isPasswordLengthGreaterThan8('123456789')
+            assert.equal(isValid, true);
+        });
+
+        it('deberia retornar falso si la contraseña tiene menos de 8 caracteres', function(){
+            let isValid = loginController.isPasswordLengthGreaterThan8('1234567')
+            assert.equal(isValid, false);
+        });
+
     });
-
-    it('debe retornar falso si el id es invalido', function(){
-      let isValid = loginController.isValidUserId(['abc123','xyz321'],'abc1234')
-      assert.equal(isValid, false);
-    });
-
-    it('deberia retornar verdadero si el email es valido', function(){
-      let isValid = loginController.isEmailValid('albeiro@gmail.com')
-      assert.equal(isValid, true);
-    });
-
-    it('deberia retornar falso si el email es invalido', function(){
-      let isValid = loginController.isEmailValid('albeiro.com')
-      assert.equal(isValid, false);
-    });
-
-  });
 
 });
